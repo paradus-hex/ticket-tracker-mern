534aa11b0798f487a31eb4e6a41fde44c0e537b4
Yaseen Nur <paradushex@gmail.com>
2023-04-10 20:17:16 +0600

    sign out

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -3,8 +3,10 @@ import { Box } from '@mui/material';
 import UserCard, { User } from '@/components/features/user/UserCard';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import ProtectedLayout from '@/components/common/ProtectedLayout';
+import { useRouter } from 'next/router';
 
 const Projects = () => {
+  const router = useRouter();
   const { data } = useCurrentUser();
 
   let clickedUser;
@@ -24,6 +26,14 @@ const Projects = () => {
           <UserCard user={clickedUser} />
         </Box>
       )}
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <button
+          className='rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20'
+          onClick={() => router.push('/')}
+        >
+          Sign Out
+        </button>
+      </div>
     </ProtectedLayout>
   );
 };
