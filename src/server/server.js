5adc9c078dd12df45507eff9e2ff9a43b9846762
Yaseen Nur <paradushex@gmail.com>
2023-04-10 11:01:06 +0600

    server seup

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,28 @@
-import express from "express";
+import cors from 'cors';
+import express from 'express';
+// import RootRouter from './routes/index.js';
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, 
+  optionSuccessStatus: 200
+};
 
-const app=express()
+const app = express();
+app.use(cors(corsOptions));
 
-app.listen(3005, async () => {
-  console.log(`API Server listening on port 3005`);
-})
+
+
+const SERVER_PORT = 3005
+
+app.use(express.json());
+
+// app.use('/api/v1', RootRouter);
+
+// for testing purposes
+app.get('/ping', (_, res) => {
+  res.send('pong');
+});
+
+app.listen(SERVER_PORT, async () => {
+  console.log(`API Server listening on port ${SERVER_PORT}`);
+});
