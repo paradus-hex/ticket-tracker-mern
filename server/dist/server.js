4975ae096e580c5a6fd534c1ae857e84c15302e4
Yaseen Nur <paradushex@gmail.com>
2023-04-10 12:31:19 +0600

    server setup

---
+++ server/dist/server.js
@@ -0,0 +1,36 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+// import RootRouter from './routes/index.js';
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+};
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)(corsOptions));
+const SERVER_PORT = process.env.SERVER_PORT;
+app.use(express_1.default.json());
+// app.use('/api/v1', RootRouter);
+// for testing purposes
+app.get('/ping', (_, res) => {
+    res.send('pong');
+});
+app.listen(SERVER_PORT, () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(`API Server listening on port ${SERVER_PORT}`);
+}));
---
+++ server/server.ts
@@ -0,0 +1,28 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+// import RootRouter from './routes/index.js';
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+};
+dotenv.config();
+
+const app = express();
+app.use(cors(corsOptions));
+
+const SERVER_PORT = process.env.SERVER_PORT;
+
+app.use(express.json());
+
+// app.use('/api/v1', RootRouter);
+
+// for testing purposes
+app.get('/ping', (_, res) => {
+  res.send('pong');
+});
+
+app.listen(SERVER_PORT, async () => {
+  console.log(`API Server listening on port ${SERVER_PORT}`);
+});
