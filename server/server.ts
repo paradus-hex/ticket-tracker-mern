31762b19be328d332387966f47874f939b0f701f
Yaseen Nur <yaseen.nur.taz@g.bracu.ac.bd>
2024-04-21 12:00:37 +0600

    ..

--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import RootRouter from './routes/index.js';
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://ticket-tracker-mern.vercel.app'],
+  origin: '*',
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200
 };
